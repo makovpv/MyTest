f6a2eda303ba43eaf71b9cabcaadfa1df5e44e05
makovpv <makovpv@gmail.com>
2016-11-18 16:42:42 +0800

    timer and textarea

--- AngularTest/myApp.js
+++ AngularTest/myApp.js
@@ -1,5 +1,5 @@
 var app = angular.module('myApp',[]);
-app.controller('myCtrl', function($scope) 
+app.controller('myCtrl', function($scope, $interval) 
 {
 		$scope.blocks = [
 			{instr:'инструкция к первому блоку вопросов', time_limit:0, random_ans_ord:0},
@@ -7,19 +7,22 @@ app.controller('myCtrl', function($scope)
 		];
 		
 		$scope.items = [
-			{text:'first question',id:10, dim: {type:1, mode:2, sel:0, subscl:[
+			{text:'first question',id:10, dim: {type:1, mode:2, sel:0, time_restrict:1,
+				subscl:[
 				{txt:"var 1",id:111},
 				{txt:"var 2",id:112},
 				{txt:"var 3",id:113}
 				]}
 			},
-			{text:'second question',id:11, dim: {type:1, mode:1, sel:0, subscl:[
+			{text:'second question',id:11, dim: {type:1, mode:1, sel:0, 
+				subscl:[
 				{txt:"var 11",id:211},
 				{txt:"var 22",id:212},
 				{txt:"var 33",id:213}
 				]},
 			},
-			{text:'third question',id:12, dim: {type:2, mode:2, sel:0, subscl:[
+			{text:'third question',id:12, dim: {type:2, mode:2, sel:0, 
+				subscl:[
 				{txt:"var zzzzz",id:311,val:true},
 				{txt:"var bbbbb",id:312,val:false},
 				{txt:"var dddd rrr tt",id:313,val:false}
@@ -35,6 +38,7 @@ app.controller('myCtrl', function($scope)
 		{
 			//alert ('click');
 			$scope.ci_index = 0;
+			$interval.cancel(timer);
 		}
 		$scope.btnClickFun = function(arg) 
 		{
@@ -46,6 +50,27 @@ app.controller('myCtrl', function($scope)
 		$scope.ci_index = 0;
 		$scope.item_vis = 1;
 		$scope.variant_vis = 1;
+		//timer
+		$scope.counter = 60;
+		var timer =	$interval
+			(
+			function()
+				{
+					if ($scope.counter > 10)
+						$scope.counter--;
+					else
+						$interval.cancel(timer);
+				},
+				1000
+			);
+		
+		//$scope.onTimeout=function () {
+			//$scope.counter++;
+			//mytimeout = $interval ($scope.onTimeout, 1000);
+			//};
+		//var mytimeout = angular.interval ($scope.onTimeout, 1000);
+		//var ver = angular.version.full;
+		
 });
 
 	
